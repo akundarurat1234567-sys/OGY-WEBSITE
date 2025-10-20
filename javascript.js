06d06d7255aa577cafaa07118431b4edbde5412c
darurat <akundarurat.1.2.3.4.5.6.7@gmail.com>
2025-10-20 13:50:08 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+  
+   function navbar() {
+  fetch('navbar.html')
+      .then(Response => Response.text())
+      .then(data =>{
+        document.getElementById('navbar-placeholder').innerHTML = data;
+
+      });
+ 
+     
+        
+      }
